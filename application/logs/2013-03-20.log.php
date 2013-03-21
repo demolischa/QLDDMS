f2c843f1cc46e86efefbeb628909fa33f70619c1
Mischa Colley <staging+mischa@exposecreative.com>
2013-03-20 23:22:54 -0500

    new site generated files and uploads

---
+++ application/logs/2013-03-20.log.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2013-03-20 21:13:26 +00:00 --- error: core.uncaught_exception
+2013-03-20 21:13:50 +00:00 --- error: core.uncaught_exception
